a0ed250ac33841584e10792313e1fb279811393f
user.name "bethelhem-kassaw <user.email "bettykassaw@gmail.com>
2023-11-28 16:41:48 +0300

    vanskape before start only navbar done

--- Vanskap/main.js
+++ Vanskap/main.js
@@ -1,25 +1,10 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs text-3xl font-bold underline">
-      Click on the Vite logo to learn more
-    </p>
-    
-  </div>
-`
+const menuBar = document.querySelector("[data-menuBar]");
+const dataNavigation = document.querySelector("[data-navigation]");
 
-setupCounter(document.querySelector('#counter'))
+menuBar.addEventListener("click", () => {
+  // console.log("itsworking");
+
+  dataNavigation.classList.toggle("active");
+});
--- Vanskap/tailwind.config.js
+++ Vanskap/tailwind.config.js
@@ -4,12 +4,42 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./main.js",
-    "./*.{js,ts,jsx,tsx,css}"
-
+    "./*.{js,ts,jsx,tsx,css}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0f1520",
+        primaryHover: "#0e131d",
+        primaryLight: "#e7e8e9",
+        primaryDark: "#0b1018",
+        primaryLightHover: "#dbdcde",
+        primaryActive: "#0c111a",
+        primaryLightActive: "#b5b6ba",
+
+        secondary: "#b28a5b",
+        secondaryHover: "#a07c52",
+        secondaryLight: "#f7f3ef",
+        secondaryDark: "##866844",
+        secondaryLightHover: "#f3ede6",
+        secondaryActive: "#8e6e49",
+        secondaryLightActive: "#e7dbcc",
+
+        tertiary: "#ecece9",
+        tertiaryHover: "#d4d4d2",
+        tertiaryLight: "#fdfdfd",
+        tertiaryDark: "#b1b1af",
+        tertiaryLightHover: "#fcfcfc",
+        tertiaryActive: "#bdbdba",
+        tertiaryLightActive: "#f9f9f8",
+      },
+      gridTemplateColumns: {
+        cards: "repeat(auto-fit, minimax(230px, 1fr))",
+      },
+      fontFamily: {
+        BLKCHCRY: ["BLKCHCRY"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
